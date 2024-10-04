3b5781d1b1de624782ec96e99e81347752623aa5
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-10-04 08:00:57 +0100

    Worked

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add the following configuration for externalizing react-router-dom
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+     
+      external: [],
     },
   },
 });
